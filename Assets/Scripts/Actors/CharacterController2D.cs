2016b91bf7523df1d757e17d34c21777dad3768d
staticMyloo <myleshosken@Hotmail.com>
2022-09-13 15:46:01 +1200

    Updating Aim Vector Parameters.

--- Assets/Scripts/Actors/CharacterController2D.cs
+++ Assets/Scripts/Actors/CharacterController2D.cs
@@ -17,7 +17,7 @@ public class CharacterController2D : MonoBehaviour
     private bool m_Grounded;            // Whether or not the player is grounded.
     const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
     private Rigidbody2D m_Rigidbody2D;
-    public bool m_FacingRight = true;  // For determining which way the player is currently facing.
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
     private Vector3 m_Velocity = Vector3.zero;
     private float moveInput = 0; // Addition
 
