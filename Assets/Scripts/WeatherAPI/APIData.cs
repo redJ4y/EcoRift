f07b4d7843dc6625dfd4c440e9c74116b204630b
Moarsome <moarsomecoarsome@gmail.com>
2022-09-21 19:37:15 +1200

    Added death for the player

--- Assets/Scripts/WeatherAPI/APIData.cs
+++ Assets/Scripts/WeatherAPI/APIData.cs
@@ -9,6 +9,6 @@ public class APIData
 
     static public string lat = "-36.852095";
     static public string lon = "174.7631803";
-    static private string key = "REMOVED API KEY";
+    static private string key = "1c69bc2783cad2acbebae2820882055b";
     static public string APIurl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + key;
 }
