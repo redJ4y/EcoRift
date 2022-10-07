2dadc19b1eee95e7d869b7e54b2011bd8ca12463
Jared Scholz <jaredsc108@gmail.com>
2022-10-08 12:05:27 +1300

    Merged and cleaned up

--- Assets/Scripts/Actors/CharacterController2D.cs
+++ Assets/Scripts/Actors/CharacterController2D.cs
@@ -6,19 +6,16 @@ using UnityEngine.Events;
 public class CharacterController2D : MonoBehaviour
 {
     [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
-    [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
     [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
     [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
     [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
     [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
     [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
-    [SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
-    [SerializeField] public bool currentlyTeleporting;                         // A boolean determining whether player is currently teleporting
+    [SerializeField] public bool currentlyTeleporting;                          // A boolean determining whether player is currently teleporting
     [SerializeReference] private Animator animator; // Addition
 
     const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
     private bool m_Grounded;            // Whether or not the player is grounded.
-    const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
     private Rigidbody2D m_Rigidbody2D;
     private bool m_FacingRight = true;  // For determining which way the player is currently facing.
     private Vector3 m_Velocity = Vector3.zero;
@@ -31,9 +28,6 @@ public class CharacterController2D : MonoBehaviour
 
     [System.Serializable]
     public class BoolEvent : UnityEvent<bool> { }
-
-    public BoolEvent OnCrouchEvent;
-    private bool m_wasCrouching = false;
 
     private SpriteRenderer renderer;
     private SwitchStaff switchStaffScript;
@@ -77,14 +71,10 @@ public class CharacterController2D : MonoBehaviour
 
         if (OnLandEvent == null)
             OnLandEvent = new UnityEvent();
-
-        if (OnCrouchEvent == null)
-            OnCrouchEvent = new BoolEvent();
     }
 
     private void Update() // Addition
     {
-        animator.SetBool("Run", Mathf.Abs(moveInput) > 0.0001f);
         animator.SetBool("Jump", !m_Grounded);
     }
 
@@ -112,56 +102,16 @@ public class CharacterController2D : MonoBehaviour
         {
             transform.localPosition = new Vector3(0, 0, 0); // (0, 0, 0) is spawn (can be changed)
         }
-        
+
     }
 
 
-    public void Move(float move, bool crouch, bool jump)
+    public void Move(float move, bool jump)
     {
         moveInput = move; // Addition
-        // If crouching, check to see if the character can stand up
-        if (!crouch)
-        {
-            // If the character has a ceiling preventing them from standing up, keep them crouching
-            if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-            {
-                crouch = true;
-            }
-        }
-
         //only control the player if grounded or airControl is turned on
         if (m_Grounded || m_AirControl)
         {
-
-            // If crouching
-            if (crouch)
-            {
-                if (!m_wasCrouching)
-                {
-                    m_wasCrouching = true;
-                    OnCrouchEvent.Invoke(true);
-                }
-
-                // Reduce the speed by the crouchSpeed multiplier
-                move *= m_CrouchSpeed;
-
-                // Disable one of the colliders when crouching
-                if (m_CrouchDisableCollider != null)
-                    m_CrouchDisableCollider.enabled = false;
-            }
-            else
-            {
-                // Enable the collider when not crouching
-                if (m_CrouchDisableCollider != null)
-                    m_CrouchDisableCollider.enabled = true;
-
-                if (m_wasCrouching)
-                {
-                    m_wasCrouching = false;
-                    OnCrouchEvent.Invoke(false);
-                }
-            }
-
             // Move the character by finding the target velocity
             Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
             // And then smoothing it out and applying it to the character
@@ -189,10 +139,8 @@ public class CharacterController2D : MonoBehaviour
         }
     }
 
-
     private void Flip()
     {
-        
         // Switch the way the player is labelled as facing.
         m_FacingRight = !m_FacingRight;
         renderer.flipX = !renderer.flipX;
@@ -207,7 +155,7 @@ public class CharacterController2D : MonoBehaviour
         if (flashing == false)
         {
             flashing = true;
-            StartCoroutine(flashColour(renderer));
+            StartCoroutine(FlashColour(renderer));
         }
     }
 
@@ -216,11 +164,11 @@ public class CharacterController2D : MonoBehaviour
         if (slowed == false)
         {
             slowed = true;
-            StartCoroutine(overlayColour(renderer, "Light Blue"));
+            StartCoroutine(OverlayColour(renderer, "Light Blue"));
         }
     }
 
-    private IEnumerator flashColour(SpriteRenderer spriteRenderer)
+    private IEnumerator FlashColour(SpriteRenderer spriteRenderer)
     {
         Color currentColor = spriteRenderer.color;
         Color32 newColor = colourReference["Red"];
@@ -236,7 +184,7 @@ public class CharacterController2D : MonoBehaviour
         flashing = false;
     }
 
-    private IEnumerator overlayColour(SpriteRenderer spriteRenderer, string newColour)
+    private IEnumerator OverlayColour(SpriteRenderer spriteRenderer, string newColour)
     {
         bool staySlowed = true;
         Color currentColor = spriteRenderer.color;
