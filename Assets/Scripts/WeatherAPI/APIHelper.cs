74bdddc5314bf08e3861972de8286c8fd16fe7de
Jared Scholz <jaredsc108@gmail.com>
2022-10-16 17:03:51 +1300

    Started weather unit test

--- Assets/Scripts/WeatherAPI/APIHelper.cs
+++ Assets/Scripts/WeatherAPI/APIHelper.cs
@@ -14,7 +14,7 @@ public class APIHelper : MonoBehaviour
         StartCoroutine(MakeRequests());
     }
 
-    private IEnumerator MakeRequests()
+    public IEnumerator MakeRequests()
     {
         // GET
         UnityWebRequest getRequest = CreateRequest(APIData.APIurl);
@@ -25,7 +25,7 @@ public class APIHelper : MonoBehaviour
         // Trigger continuation of game flow
     }
 
-    private UnityWebRequest CreateRequest(string path, RequestType type = RequestType.GET, object data = null)
+    public UnityWebRequest CreateRequest(string path, RequestType type = RequestType.GET, object data = null)
     {
         UnityWebRequest request = new UnityWebRequest(path, type.ToString());
 
