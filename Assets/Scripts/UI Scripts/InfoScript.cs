0b8edf4fc8a0d084cb5cd346838ca4336052ab14
Moarsome <moarsomecoarsome@gmail.com>
2022-10-19 21:13:18 +1300

    Implemented info GUI, balanced weapons

--- Assets/Scripts/UI Scripts/InfoScript.cs
+++ Assets/Scripts/UI Scripts/InfoScript.cs
@@ -7,6 +7,8 @@ public class InfoScript : MonoBehaviour
 {
     [SerializeField] private Vector3 startingPos;
     [SerializeField] private Vector3 endingPos;
+    [SerializeField] private float duration;
+    [SerializeField] private float animationSpeed = 2.0f;
     [SerializeReference] private GameObject textObj;
 
     public void Alert(string message)
@@ -20,22 +22,21 @@ public class InfoScript : MonoBehaviour
     private IEnumerator moveSmoothly(TMP_Text tmp, Transform tra)
     {
         var t = 0f;
-        float speed = 2.0f; 
 
         while (t < 1.0f)
         {
-            t += Time.deltaTime * speed;
+            t += Time.deltaTime * animationSpeed;
             tmp.fontMaterial.SetColor("_FaceColor", Color.Lerp(new Color(1.0f, 1.0f, 1.0f, 0.0f), Color.white, t));
             tra.localPosition = Vector3.Lerp(startingPos, endingPos, t);
             yield return null;
         }
 
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(duration);
 
         t = 0f;
         while (t < 1.0f)
         {
-            t += Time.deltaTime * speed;
+            t += Time.deltaTime * animationSpeed;
             tmp.fontMaterial.SetColor("_FaceColor", Color.Lerp(Color.white, new Color(1.0f, 1.0f, 1.0f, 0.0f), t));
             yield return null;
         }
